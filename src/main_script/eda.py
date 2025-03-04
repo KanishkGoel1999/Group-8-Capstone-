95fe0a38d47a32f7b1e092633c6f2a7f5d73d761
Shikha Kumari <shikha.2893@gmail.com>
2025-03-04 16:21:37 -0500

    changes

--- src/main_script/eda.py
+++ src/main_script/eda.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     file_path = "../data/preprocessed_data.csv"
     eda = EDA(file_path)
     eda.load_data()
-    eda.preprocess_data()
+    eda.preprocess_data(["user_id", "reputation", "display_name"])
     eda.plot_heatmap()
