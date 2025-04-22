8867781a5b39a346ba1ce5936cc545414157e018
Kanishk Goel <46555737+KanishkGoel1999@users.noreply.github.com>
2025-04-22 18:24:34 -0400

    Add files via upload

--- code/main_code/classical_machine_learning_models/test_ML.py
+++ code/main_code/classical_machine_learning_models/test_ML.py
@@ -1,84 +1,58 @@
-import os
-import argparse
-import numpy as np
-import pandas as pd
-from tqdm import tqdm
-import time
 
-import prettytable
-
-import sys
-from matplotlib import pyplot as plt
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 from component.preprocess import *
 from component.classical_machine_learning_models.utils import *
-
-import argparse
-import warnings
-
+from component.packages import *
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 warnings.filterwarnings("ignore")
 
-#data_path = '/home/ubuntu/processed_test.csv'
-data_path = '/home/ubuntu/synthetic_fraud_data.csv'
 
-def generate_aucpr_plot(y_test, y_prob_dict):
-    plt.figure(figsize=(8, 6))
-    for model_name, y_prob in y_prob_dict.items():
-        if y_prob.shape[1] == 2:
-            y_prob = y_prob[:, 1]
-        precision, recall, _ = precision_recall_curve(y_test, y_prob)
-        plt.plot(recall, precision, label=f'{model_name} (AUC={average_precision_score(y_test, y_prob):.2f})')
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.title('Precision-Recall Curve')
-    plt.legend()
-    plt.savefig('plots/pr_curve.png')
-    plt.show()
-
-def generate_aucroc_plot(y_test, y_prob_dict):
-    plt.figure(figsize=(8, 6))
-    for model_name, y_prob in y_prob_dict.items():
-        if y_prob.shape[1] == 2:
-            y_prob = y_prob[:, 1]
-        fpr, tpr, _ = roc_curve(y_test, y_prob)
-        plt.plot(fpr, tpr, label=f'{model_name} (AUC={roc_auc_score(y_test, y_prob):.2f})')
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('ROC Curve')
-    plt.legend()
-    plt.savefig('plots/roc_curve.png')
-    plt.show()
+def load_config(config_path="/home/ubuntu/code/component/config.yaml"):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    return config
 
 
 def evaluate():
-    target = 'is_fraud'
-
-    # Load test data
+    config = load_config()
+    target = config['ml']['target']
     print("Loading test data...")
+
+    # For dataset 1
+    data_path = config['data']['test_data_path1']
+    data_path = os.path.join(os.path.expanduser("~"), data_path)
+    dir_name = "models_DS1"
     data = pd.read_csv(data_path)
+    data = preprocess_data_1(data, 'trans_date_trans_time', 'merchant', 'trans_num')
+    data.drop(['card_number', 'fraud_merchant_pct', 'merchant_id', 'transaction_id'],
+            axis=1, inplace=True)
 
-    #data = preprocess_data_ML(data, 'trans_date_trans_time', 'merchant', 'trans_num')
-    #data = preprocess_data2(data, 'timestamp', 'merchant', 'transaction_id')
-    train, data = preprocessDS2((data))
 
-    data = data[
-        ['transaction_id', 'amount', 'weekend_transaction', 'hour_sin', 'hour_cos', 'day_sin', 'day_cos',
-         'currency_AUD', 'currency_CAD', 'currency_EUR', 'currency_GBP', 'currency_JPY', 'currency_RUB', 'currency_SGD',
-         'currency_USD', 'device_Android App', 'device_Chip Reader', 'device_Magnetic Stripe', 'device_NFC Payment',
-         'device_Safari', 'device_iOS App', 'card_number', 'country_Canada', 'country_France',
-         'country_Germany', 'country_Japan', 'country_Russia', 'country_Singapore', 'country_UK',
-         'country_USA', 'merchant_id', 'merchant_category_Education', 'merchant_category_Entertainment',
-         'merchant_category_Gas', 'merchant_category_Grocery', 'is_fraud']]
+    # For dataset 2
+    # data_path = config['data']['data_path2']
+    # data_path = os.path.join(os.path.expanduser("~"), data_path)
+    #
+    # # Load test data
+    # data = pd.read_csv(data_path)
+    # train, data = preprocess_data_2((data))
+    # dir_name = "models_DS2"
+    # data = data[
+    #     ['transaction_id', 'amount', 'weekend_transaction', 'hour_sin', 'hour_cos', 'day_sin', 'day_cos',
+    #      'currency_AUD', 'currency_CAD', 'currency_EUR', 'currency_GBP', 'currency_JPY', 'currency_SGD', 'currency_USD',
+    #      'device_Android App', 'device_Chip Reader', 'device_Magnetic Stripe', 'device_NFC Payment', 'card_number',
+    #      'country_Canada', 'country_France',
+    #      'country_Germany', 'country_Japan', 'country_Russia', 'country_Singapore', 'country_UK',
+    #      'country_USA', 'merchant_id', 'merchant_category_Education', 'merchant_category_Entertainment',
+    #      'merchant_category_Gas', 'merchant_category_Grocery', 'is_fraud']]
 
-    print(data.info())
+
+    print("Testing starts....")
     X_test = data.drop(columns=[target])
     y_test = data[target]
 
-    results = test_model(X_test, y_test)
+    results = test_model(X_test, y_test, dir_name)
 
     print(results)
-
 
 if __name__ == "__main__":
     evaluate()
--- code/main_code/classical_machine_learning_models/train_ML.py
+++ code/main_code/classical_machine_learning_models/train_ML.py
@@ -1,6 +1,8 @@
 # train_models.py
 import sys
 
+import yaml
+
 sys.path.append('../../component')
 
 import os
@@ -23,47 +25,13 @@ import torch
 from component.preprocess import *
 from component.classical_machine_learning_models.utils import *
 
-#data_path = '/home/ubuntu/processed_train.csv'
-data_path = '/home/ubuntu/synthetic_fraud_data.csv'
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-seed = 42
 
-np.random.seed(seed)
-parent_dir = os.getcwd()
-
-
-
-def save_predictions(model, X_test, y_test, output_path):
-    '''
-    Generate predictions using the trained model and save them to a file along with the original dataframe
-
-    :param model: Trained model
-    :param X_test: Test features
-    :param y_test: Test target
-    :param output_path: Path to save the predictions
-
-    :returns None
-    '''
-    try:
-        y_pred = model.predict(X_test)
-        y_prob = model.predict_proba(X_test)[:, 1]
-
-        y_pred = (y_prob > 0.5).astype(int)
-        predictions_df = pd.concat([X_test.reset_index(drop=True), y_test.reset_index(drop=True)], axis=1)
-        predictions_df['Prediction'] = y_pred
-        predictions_df['Probability'] = y_prob
-
-        if not os.path.exists(os.path.dirname(output_path)):
-            os.makedirs(os.path.dirname(output_path))
-
-        predictions_df.to_csv(output_path, index=False)
-
-        print(f"Predictions saved to {output_path}")
-
-    except Exception as e:
-        print(f"Error generating predictions: {e}")
+def load_config(config_path="/home/ubuntu/code/component/config.yaml"):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    return config
 
 
 def save_model(model, model_path):
@@ -89,24 +57,45 @@ def save_model(model, model_path):
 
 
 def main():
-    target = 'is_fraud'
+    config = load_config()
+    target = config['ml']['target']
+    seed = config
+
+    np.random.seed(seed)
+
+    # For dataset 1
+    data_path = config['data']['train_data_path1']
+    data_path = os.path.join(os.path.expanduser("~"), data_path)
     data = pd.read_csv(data_path)
+    dir_name = "models_DS1"
+    data = preprocess_data_1(data, 'trans_date_trans_time', 'merchant', 'trans_num')
+    data.drop(['card_number', 'fraud_merchant_pct', 'merchant_id', 'transaction_id'],
+            axis=1, inplace=True)
 
-    #data = preprocess_data_ML(data, 'trans_date_trans_time', 'merchant', 'trans_num')
-    data, test = preprocessDS2(data)
 
-    data = data[
-        ['transaction_id', 'amount', 'weekend_transaction', 'hour_sin', 'hour_cos', 'day_sin', 'day_cos',
-         'currency_AUD', 'currency_CAD', 'currency_EUR', 'currency_GBP', 'currency_JPY', 'currency_RUB', 'currency_SGD', 'currency_USD', 'device_Android App', 'device_Chip Reader', 'device_Magnetic Stripe', 'device_NFC Payment',
-         'device_Safari', 'device_iOS App', 'card_number', 'country_Canada', 'country_France',
-         'country_Germany', 'country_Japan', 'country_Russia', 'country_Singapore', 'country_UK',
-         'country_USA', 'merchant_id', 'merchant_category_Education', 'merchant_category_Entertainment', 'merchant_category_Gas', 'merchant_category_Grocery', 'is_fraud']]
+    # For dataset 2
+    # data_path = config['data']['data_path2']
+    # data_path = os.path.join(os.path.expanduser("~"), data_path)
+    # data = pd.read_csv(data_path)
+    # dir_name = "models_DS2"
+    # data, test = preprocess_data_2(data)
+    # data = data[
+    #     ['transaction_id', 'amount', 'weekend_transaction', 'hour_sin', 'hour_cos', 'day_sin', 'day_cos',
+    #      'currency_AUD', 'currency_CAD', 'currency_EUR', 'currency_GBP', 'currency_JPY', 'currency_SGD', 'currency_USD',
+    #      'device_Android App', 'device_Chip Reader', 'device_Magnetic Stripe', 'device_NFC Payment', 'card_number',
+    #      'country_Canada', 'country_France',
+    #      'country_Germany', 'country_Japan', 'country_Russia', 'country_Singapore', 'country_UK',
+    #      'country_USA', 'merchant_id', 'merchant_category_Education', 'merchant_category_Entertainment',
+    #      'merchant_category_Gas', 'merchant_category_Grocery', 'is_fraud']]
 
     X = data.drop(columns=[target])
     y = data[target]
 
-    train_models(X, y)
+    print("Training starts")
+    train_models(X, y,dir_name)
 
 
 if __name__ == '__main__':
+
+
     main()
