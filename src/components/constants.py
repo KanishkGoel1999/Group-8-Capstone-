ad192d6ae72518472d1ed76b14345c1588879680
Shikha Kumari <shikha.2893@gmail.com>
2025-04-22 12:29:22 -0400

    code updated for GNN train and test

--- src/components/constants.py
+++ src/components/constants.py
@@ -1,21 +1,3 @@
-# import os
-# import sys
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-# from components.utils import parse_args
-
-# Edge Types
-EDGE_TYPES_DICT = {
-    'ASKS': ('user', 'asks', 'question'),
-    'REV_ASKS': ('question', 'rev_asks', 'user'),
-    'ANSWERS': ('user', 'answers', 'answer'),
-    'REV_ANSWERS': ('answer', 'rev_answers', 'user'),
-    'HAS': ('question', 'has', 'answer'),
-    'REV_HAS': ('answer', 'rev_has', 'question'),
-    'ACCEPTED_ANSWER': ('question', 'accepted_answer', 'answer'),
-    'REV_ACCEPTED': ('answer', 'rev_accepted', 'question'),
-    'SELF_LOOP': ('user', 'self_loop', 'user')
-}
-
 DATASET_TYPE_1 = 'STACK_OVERFLOW'
 DATASET_TYPE_2 = 'ASK_REDDIT'
 
